9a297abad6e088f65ef8911c1ccf3f9a837f6466
Erik Knaake <e.knaake@student.han.nl>
2019-05-23 18:27:32 +0200

    Fixed an issue where non production build would not render properly

--- EKVault/src/app/navigation/navigation.component.ts
+++ EKVault/src/app/navigation/navigation.component.ts
@@ -1,12 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {NavigationEnd, Router, RouterEvent} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   constructor(public readonly router: Router) { }
 
+  ngOnInit() {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if(event instanceof NavigationEnd)
+        console.log(event);
+    });
+  }
 }
